0b5929f585ba3b3683420d7c1b5785879fb251ca
marshalexander99 <55112990+marshalexander99@users.noreply.github.com>
2022-10-24 19:42:27 +0100

    Create configure.ps1

---
+++ configure.ps1
@@ -0,0 +1,210 @@
+#    WVD Script Parameters   #
+##############################
+Param (
+    [parameter]
+        [string]$redirxml,
+    [Parameter(Mandatory=$true)]
+        [string]$RegistrationToken,
+        [string]$ProfilePath, 
+        [string]$aadjoin          
+)
+
+#    WVD Variables   #
+$LocalWVDpath            = "c:\temp\wvd\"
+$WVDBootURI              = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+$WVDAgentURI             = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+$WVDAgentInstaller       = 'WVD-Agent.msi'
+$WVDBootInstaller        = 'WVD-Bootloader.msi'
+
+$aadjoin = [System.Convert]::ToBoolean($aadjoin)
+
+#create directory
+if((Test-Path c:\temp) -eq $false) {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating temp directory"
+    New-Item -Path c:\temp -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "temp directory already exists"
+}
+if((Test-Path $LocalWVDpath) -eq $false) {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp\WVD Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating c:\temp\wvd directory"
+    New-Item -Path $LocalWVDpath -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp\WVD Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "c:\temp\wvd directory already exists"
+}
+New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
+Add-Content `
+-LiteralPath C:\New-WVDSessionHost.log `
+"
+ProfilePath       = $ProfilePath
+RegistrationToken = $RegistrationToken
+"
+
+#download wvd components
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading WVD Boot Loader"
+    Invoke-WebRequest -Uri $WVDBootURI -OutFile "$LocalWVDpath$WVDBootInstaller"
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading WVD Agent"
+    Invoke-WebRequest -Uri $WVDAgentURI -OutFile "$LocalWVDpath$WVDAgentInstaller"
+
+#    Install WVD Componants    #
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Bootloader"
+$bootloader_deploy_status = Start-Process `
+    -FilePath "msiexec.exe" `
+    -ArgumentList "/i $LocalWVDpath$WVDBootInstaller", `
+        "/quiet", `
+        "/qn", `
+        "/norestart", `
+        "/passive", `
+        "/l* $LocalWVDpath\AgentBootLoaderInstall.txt" `
+    -Wait `
+    -Passthru
+$sts = $bootloader_deploy_status.ExitCode
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Bootloader Complete"
+Write-Output "Installing RDAgentBootLoader on VM Complete. Exit code=$sts`n"
+Wait-Event -Timeout 5
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Agent"
+Write-Output "Installing RD Infra Agent on VM $AgentInstaller`n"
+#$agent_deploy_status = Start-Process `
+    -FilePath "msiexec.exe" `
+    -ArgumentList "/i $LocalWVDpath$WVDAgentInstaller", `
+        "/quiet", `
+        "/qn", `
+        "/norestart", `
+        "/passive", `
+        "REGISTRATIONTOKEN=$RegistrationToken", "/l* $LocalWVDpath\AgentInstall.txt" `
+    -Wait `
+    -Passthru
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "WVD Agent Install Complete"
+Wait-Event -Timeout 5
+
+#    Enable Screen Capture Protection    #
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Enable Screen Capture Protection"
+Push-Location 
+Set-Location "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+New-ItemProperty `
+    -Path .\ `
+    -Name fEnableScreenCaptureProtection `
+    -Value "1" `
+    -PropertyType DWord `
+    -Force
+Pop-Location
+
+#######################################
+#    FSLogix User Profile Settings    #
+#######################################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Configure FSLogix Profile Settings"
+Push-Location 
+Set-Location HKLM:\SOFTWARE\
+New-Item `
+    -Path HKLM:\SOFTWARE\FSLogix `
+    -Name Profiles `
+    -Force
+New-Item `
+    -Path HKLM:\Software\FSLogix\Profiles\ `
+    -Name Apps `
+    -Force
+New-ItemProperty `
+    -path KLM:\Software\FSLogix\Apps `
+    -name "CleanupInvalidSessions" `
+    -type "Dword" `
+    -value "1" `
+    -force
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "Enabled" `
+    -Type "Dword" `
+    -Value "1"
+New-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "VHDLocations" `
+    -Value "$ProfilePath" `
+    -PropertyType MultiString `
+    -Force
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "SizeInMBs" `
+    -Type "Dword" `
+    -Value "30000"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "IsDynamic" `
+    -Type "Dword" `
+    -Value "1"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "VolumeType" `
+    -Type String `
+    -Value "vhdx"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "FlipFlopProfileDirectoryName" `
+    -Type "Dword" `
+    -Value "1" 
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "SIDDirNamePattern" `
+    -Type String `
+    -Value "%username%%sid%"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "SIDDirNameMatch" `
+    -Type String `
+    -Value "%username%%sid%"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name DeleteLocalProfileWhenVHDShouldApply `
+    -Type DWord `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "PreventLoginWithFailure" `
+    -Value "1" `
+    -PropertyType "DWORD" `
+    -Force
+New-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "RedirXMLSourceFolder" `
+    -Value $redirxml `
+    -PropertyType REG_SZ `
+    -Force
+Pop-Location
+
+
+##############################
+#    Enable Azure AD Join    #
+##############################
+if ($aadjoin){
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Enable Azure AD Join"
+Push-Location 
+Set-Location HKLM:\SOFTWARE\Microsoft
+New-Item `
+    -Path HKLM:\SOFTWARE\Microsoft `
+    -Name RDInfraAgent `
+    -Force
+New-Item `
+    -Path HKLM:\Software\Microsoft\RDInfraAgent `
+    -Name AADJPrivate `
+    -Force
+Pop-Location
+}
+
+#    Restart Computer    #
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Process Complete - REBOOT"
+Restart-Computer -Force 
